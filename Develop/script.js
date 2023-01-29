36964ef3201d67c3ae05b564ef0848ae429a7fd8
Hinalpatel8988 <118151523+Hinalpatel8988@users.noreply.github.com>
2023-01-29 20:48:55 +1100

    local storage added to the application

--- Develop/script.js
+++ Develop/script.js
@@ -8,12 +8,13 @@ function updateTimer() {
 
 setInterval(updateTimer, 1000);
 
-
 $("#currentDay").text(today.format("dddd, MMMM D, YYYY [at] hh:mm:ss a"));
 updateTimer();
 
 var hour = $(".hour");
 var description = $(".description");
+
+function timeDisplaycolor() {
 
 for (var i = 0; i < hour.length; i++) {
 if (+hour.eq(i).attr("data-index") === +today.format("H")) {
@@ -22,8 +23,9 @@ if (+hour.eq(i).attr("data-index") === +today.format("H")) {
   description.eq(i).addClass("past");
 }  else {
   description.eq(i).addClass("future");
+}}
 }
-}
+timeDisplaycolor()
 
 $(".btn").click(function (event) {
   event.preventDefault();
@@ -31,6 +33,27 @@ $(".btn").click(function (event) {
   var hour = $(this).parent().attr("id");
   localStorage.setItem(hour, value);
 });
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
+
+
+
+
+
+
+
+
 
 
 
